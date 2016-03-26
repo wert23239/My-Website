ca4ad1f129b322ca11b9b984add33097c18dc6b4
wert23239 <lambe168@msu.edu>
2016-03-25 22:54:43 -0400

    Renamed and Made some default classes

---
+++ Code/EmptyCode.php
@@ -0,0 +1,17 @@
+<?php
+require 'lib/Site/View.php';
+$view= new Site\SiteView();
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 3/25/2016
+ * Time: 10:48 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php echo $view->presenthead() ?>
+<body>
+<?php echo $view->presentheader() ?>
+
+</body>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'lib/Site/View.php';
+$view= new Site\SiteView();
+?>
+
+<!doctype html>
+<html lang='en'>
+<?php echo $view->presenthead() ?>
+<body>
+<?php echo $view->presentheader() ?>
+<h1>Hi I'm Alex,</h1>
+<p>Programmer, Artist, and Author</p>
+
+<p>Born in Michigan. Senior at Michigan State University Studying Computer Science with a Cognitive in Psychology. </p>
+<p>I'm been doing Research for <a href="#">Arends Hintze Lab</a> and work for <a href="#">Sparrow Health Insurance</a> in the IT Department.</p>
+</body>
+</html>
