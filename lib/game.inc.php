292dc45f5989594644fd73adbc2571a54266c786
wert23239 <lambe168@msu.edu>
2016-03-25 22:32:28 -0400

    Added View

--- lib/game.inc.php
+++ lib/game.inc.php
@@ -8,7 +8,7 @@
 //
 //// If there is a Wumpus session, use that. Otherwise, create one
 //if(!isset($_SESSION[WUMPUS_SESSION])) {
-//    $_SESSION[WUMPUS_SESSION] = new Wumpus\Wumpus();   // Seed: 1422668587
+//    $_SESSION[WUMPUS_SESSION] = new Site\Site();   // Seed: 1422668587
 //}
 //
 //$wumpus = $_SESSION[WUMPUS_SESSION];
