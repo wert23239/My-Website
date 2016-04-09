7a65bbe701a2f15a7bde0521c14cfc837ce8407b
wert23239 <lambe168@msu.edu>
2016-04-09 01:06:42 -0400

    Redid HomePage

--- index.php
+++ index.php
@@ -15,14 +15,15 @@ $view= new Site\View();
 <h2>Programmer, Artist, and Author</h2>
 
 <p>Born in Michigan. Senior at Michigan State University Studying Computer Science with a Cognitive in Psychology. </p>
-<p>I'm been doing Research for <a href="#">Arends Hintze Lab</a> and work for <a href="#">Sparrow Health Insurance</a> in the IT Department.</p>
-<div class="st-container">
+<p>I'm been doing Research for <a href="http://hintzelab.msu.edu/">Arends Hintze Lab</a>
+    and work for <a href="https://www.phpmichigan.com/">Sparrow Health Insurance</a> in the IT Department.</p>
+<div class="containerrep">
 
-</div><!-- // st-container -->
-<footer>
-      <span><a style="margin-left:1em;color:black;" href="https://github.com/wert23239" target="_blank"><img style="height:4em;width:4em;" src="images/github.png" alt="Github" ></a></span>
-      <span><a style="margin-left:1em;color:black;" href="https://www.linkedin.com/in/alexlambert52" target="_blank"><img style="height:4em;width:4em;" width="2em" height="2em" src="images/linkedin.png" alt="Linkedin"></a></span>
+    <span><a style="margin-left:1em;color:black;" href="https://github.com/wert23239" target="_blank"><img style="height:2em;width:2em;" src="images/github.png" alt="Github" ></a></span>
+      <span><a style="margin-left:1em;color:black;" href="https://www.linkedin.com/in/alexlambert52" target="_blank"><img style="height:2em;width:2em;" width="2em" height="2em" src="images/linkedin.png" alt="Linkedin"></a></span>
+</div>
 
-</footer>
+
+
 </body>
 </html>
