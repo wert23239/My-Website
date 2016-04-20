4c65429477bb2163f8ed59957a7e5764f4523419
wert23239 <lambe168@msu.edu>
2016-04-20 18:31:49 -0400

    Added Bad Version of Proccesing Java

--- Processing2.php
+++ Processing2.php
@@ -9,7 +9,7 @@
 <script src="node_modules/video.js/dist/video.min.js"></script>
 <div style="width:700px;margin:0px auto;">
     <video id="example_video_1" class="video-js vjs-default-skin"
-           controls preload="auto" width="640" height="264"
+           controls preload="auto" width="640" height="264" autoplay="true"
            poster="images/VirtualAlpha.png"
            data-setup='{"controls":true}'>
         <source src="ExampleMovie.m4v" type="video/mp4" />
