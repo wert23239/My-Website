635bb127bdde5695387a961f28d3188816f1aa64
wert23239 <lambe168@msu.edu>
2016-05-27 00:13:15 -0400

    Added Bootstrap

--- Vania.php
+++ Vania.php
@@ -10,10 +10,20 @@ $view= new Site\CodeView("downloads/CityGame.zip",\Site\SiteManager::VaniaNum);
 
 
 <?php echo $view->presentheader();
-$image="images/HackerRank.png";
-$text="This is a variety of difficult problems I have finished on HackerRank";
-echo $view->presentDisplay($image,$text);
 ?>
+<pre>hey</pre>
+
+<div class="container CodeA">
+    <ul class="pager">
+        <li class="previous"><a href="#">Previous</a></li>
+        <li class="next"><a href="#">Next</a></li>
+
+    </ul>
+    <h1 class="CodeB">Vania Virtual Alpha</h1>
+    <p class="CodeA2">A Virtual Evolution Project made at Mhacks Fall2016 using</p>
+    <p><img src=images/gallery.jpg id="image" width="70%" center-block"></p>
+</div>
+
 
 </body>
 </html>
